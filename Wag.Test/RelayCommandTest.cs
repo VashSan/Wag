26cc5f7b5955d14310e3a9587a17b9d9b87e6795
Alexander Schwoch <alex@palc.de>
2020-08-01 18:15:54 +0200

    tests for relay command

---
+++ Wag.Test/RelayCommandTest.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Wag.Domain;
+
+namespace Wag.Test
+{
+	public class RelayCommandTest
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Constructor_WithNullAction_Throws()
+		{
+			// Arrange & Act
+			static void Actor()
+			{
+				// ReSharper disable once ObjectCreationAsStatement
+				new RelayCommand( null );
+			}
+
+			// Assert
+			Assert.That( (Action)Actor, Throws.ArgumentNullException, "The relay command should not accept a null action" );
+		}
+
+		[TestCaseSource( nameof( CanExecuteFunctionsThatReturnTrue ) )]
+		public void CanExecute_Invocations_ReturnTrue( Func<object, bool> canExecute )
+		{
+			// Arrange
+			var command = new RelayCommand( ( o ) => { }, canExecute );
+
+			// Act
+			var result = command.CanExecute( null );
+
+			// Assert
+			Assert.That( result, Is.True );
+		}
+
+		public static IEnumerable<TestCaseData> CanExecuteFunctionsThatReturnTrue
+		{
+			get
+			{
+				yield return new TestCaseData( null );
+				yield return new TestCaseData( (Func<object, bool>)(( o ) => true) );
+			}
+		}
+
+		[Test]
+		public void CanExecute_Invocations_ReturnFalse()
+		{
+			// Arrange
+			static bool CanExecute( object o ) => false;
+			var command = new RelayCommand( ( o ) => { }, CanExecute );
+
+			// Act
+			var result = command.CanExecute( null );
+
+			// Assert
+			Assert.That( result, Is.False );
+		}
+
+		[Test]
+		public void Execute_Invocation_PerformsAction()
+		{
+			// Arrange
+			var executed = false;
+			void Execute( object o ) => executed = true;
+			var command = new RelayCommand( Execute );
+
+			// Act
+			command.Execute( null );
+
+			// Assert
+			Assert.That( executed, Is.True );
+		}
+	}
+}
--- Wag/Domain/RelayCommand.cs
+++ Wag/Domain/RelayCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Windows.Input;
 
@@ -8,29 +9,29 @@ namespace Wag.Domain
 
 	public class RelayCommand : ICommand
 	{
-		private Action<object> execute;
-		private Func<object, bool> canExecute;
+		private readonly Action<object> execute;
+		private readonly Func<object, bool> canExecute;
 
 		public event EventHandler CanExecuteChanged
 		{
-			add { CommandManager.RequerySuggested += value; }
-			remove { CommandManager.RequerySuggested -= value; }
+			add => CommandManager.RequerySuggested += value;
+			remove => CommandManager.RequerySuggested -= value;
 		}
 
 		public RelayCommand( Action<object> execute, Func<object, bool> canExecute = null )
 		{
-			this.execute = execute;
+			this.execute = execute ?? throw new ArgumentNullException( nameof( execute ) );
 			this.canExecute = canExecute;
 		}
 
 		public bool CanExecute( object parameter )
 		{
-			return this.canExecute == null || this.canExecute( parameter );
+			return canExecute == null || canExecute( parameter );
 		}
 
 		public void Execute( object parameter )
 		{
-			this.execute( parameter );
+			execute( parameter );
 		}
 	}
 }
