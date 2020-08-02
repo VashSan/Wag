158869e5bc01c7517564666c310b90d8f9827fc9
Alexander Schwoch <alex@palc.de>
2020-08-02 16:53:53 +0200

    added Moq and test for StartMenuViewModel

---
+++ Wag.Test/Domain/StartMenuViewModelTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using Wag.Domain;
+
+namespace Wag.Test.Domain
+{
+	public class StartMenuViewModelTest
+	{
+		[Test]
+		public void Query_BeingEntered_TriggersSearch()
+		{
+			// Arrange
+			var vm = new StartMenuViewModel();
+
+			// Act
+			vm.Query = "Test";
+
+			// Assert
+		}
+	}
+}
